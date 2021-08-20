db7f4abd590133ec46dcc5ce57cf5b771f21b5b1
Aman <patilaman080@gmail.com>
2021-08-20 08:50:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import ImageCompo from "./components/ImageCompo";
+
+import CardBody from "./components/CardBody";
+function App() {
+  return (
+    <div>
+      <div className="card" >
+
+        <ImageCompo />
+
+        <CardBody />
+
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ImageCompo.js
@@ -0,0 +1,9 @@
+
+
+function ImageCompo(){
+    return (
+        <img src="./her.svg" alt="..." />
+    )
+}
+
+export default ImageCompo
